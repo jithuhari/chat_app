fcddca590895f4cba67e86e617703037a3aa30bf
JithuHari <jithu@nintrivalabs.com>
2024-02-20 15:08:04 +0530

    message bottom sheet finished

--- lib/features/chat_window/chat_window_screen.dart
+++ lib/features/chat_window/chat_window_screen.dart
@@ -263,19 +263,115 @@ class ChatWindowScreen extends StatelessWidget {
           return SizedBox(
               height: MediaQuery.of(context).size.height / 2,
               width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(10)),
-                    height: 3,
-                    width: 75,
-                  )
-                ],
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(10)),
+                      height: 3,
+                      width: 75,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 24),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                              'assets/png/message_bottom_sheet/love.png'),
+                          Image.asset(
+                              'assets/png/message_bottom_sheet/laugh.png'),
+                          Image.asset(
+                              'assets/png/message_bottom_sheet/sad.png'),
+                          Image.asset(
+                              'assets/png/message_bottom_sheet/thumps_up.png'),
+                          Image.asset(
+                              'assets/png/message_bottom_sheet/add_reactions.png')
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Image.asset('assets/png/message_bottom_sheet/copy.png'),
+                        const SizedBox(
+                          width: 32,
+                        ),
+                        const Text(
+                          'Copy',
+                          style: TextStyle(
+                              color: secondaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Image.asset('assets/png/message_bottom_sheet/pin.png'),
+                        const SizedBox(
+                          width: 32,
+                        ),
+                        const Text(
+                          'Pin Message',
+                          style: TextStyle(
+                              color: secondaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Image.asset('assets/png/message_bottom_sheet/star.png'),
+                        const SizedBox(
+                          width: 32,
+                        ),
+                        const Text(
+                          'Star Message',
+                          style: TextStyle(
+                              color: secondaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(
+                            'assets/png/message_bottom_sheet/reply.png'),
+                        const SizedBox(
+                          width: 32,
+                        ),
+                        const Text(
+                          'Reply',
+                          style: TextStyle(
+                              color: secondaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(
+                            'assets/png/message_bottom_sheet/forward.png'),
+                        const SizedBox(
+                          width: 32,
+                        ),
+                        const Text(
+                          'Forward',
+                          style: TextStyle(
+                              color: secondaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ));
         });
   }
